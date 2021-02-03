1613cb2265c7ff53fe67f22cc59220237f7ea3ce
MMhenna <mhenna.mehdi@gmail.com>
2021-02-03 21:09:44 +0100

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * display message
+ * Return: Error
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", -2);
+return (1);
+}
