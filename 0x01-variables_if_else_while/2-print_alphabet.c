f8e92443f0ccc00e577909edadfcce829b58a22c
MMhenna <mhenna.mehdi@gmail.com>
2021-02-04 14:16:46 +0100

    commit 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * print alpha
+ * Return: success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	return (0);
+}
