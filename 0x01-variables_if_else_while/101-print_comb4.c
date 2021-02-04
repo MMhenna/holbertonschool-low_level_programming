f845d2f19e56a1ad3e3854d328fe4654836f73aa
MMhenna <mhenna.mehdi@gmail.com>
2021-02-04 23:54:40 +0100

    please

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,26 +9,30 @@
 int main(void)
 {
 	int c, i, j;
+	int d = '1';
+	int b = '2';
 
 	for (c = '0'; c <= '7'; c++)
 	{
-		for (i = '1'; i <= '8'; i++)
+		for (i = d; i <= '8'; i++)
 		{
-			for (j = '1'; j <= '9'; j++)
+			for (j = b; j <= '9'; j++)
 			{
-				if ((c != i) && (i != j) && (c != j))
+				if ((c != i) && (c != j) && (i != j))
 				{
 					putchar(c);
 					putchar(i);
 					putchar(j);
-					if ((c != '7') || (i != '8') || (j != '9'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				if ((c != '7') || (i != '8') || (j != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 				}
 			}
+		b++;
 		}
+	d++;
 	}
 	putchar('\n');
 	return (0);
