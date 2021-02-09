f1c7a648082642394866a365e46f548205c6acee
MMhenna <mhenna.mehdi@gmail.com>
2021-02-09 11:37:55 +0100

    commit add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - add two numbers
+ *@i: first parameter
+ *@j: second parameter
+ *Return: @i + @j
+ */
+
+int add(int i, int j)
+{
+int k = i + j;
+return(k);
+}
