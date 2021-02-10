5b9832c2bb2c8bad9f3b2f2f3e8776c83ae332ba
MMhenna <mhenna.mehdi@gmail.com>
2021-02-10 21:10:30 +0100

    multiple

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,24 +7,18 @@
  */
 int main(void)
 {
-int t = 0;
-int f = 0;
 int i = 3;
-int j = 5;
-
+int s = 0;
 while (i <= 1024)
 {
-	t = t + i;
-	i = i + 3;
-}
-while (j <= 1024)
-{
-	f = f + j;
-	j = j + 5;
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+		s = s + i;
+	}
+i++;
 }
 
-printf("sum of the multiples of 3 below 1024 : %d\n", t);
-printf("sum of the multiples of 5 below 1024 : %d\n", f);
+printf("sum of the multiples of 3 and 5 below 1024 : %d\n", s);
 
 return (0);
 }
