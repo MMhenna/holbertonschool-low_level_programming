c25848efbb8f032a75a886ee13461d98c58ae91c
MMhenna <mhenna.mehdi@gmail.com>
2021-02-11 14:56:04 +0100

    2 com

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,26 +13,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 != 0))
-		{
-			printf("Fizz");
-			printf(" ");
-		}
+			printf("Fizz ");
 		else if ((i % 5 == 0) && (i % 3 != 0))
-		{
-			printf("Buzz");
-			printf(" ");
-		}
+			printf("Buzz ");
 		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz");
-			printf(" ");
-		}
+			printf("FizzBuzz ");
 		else
-		{
-			printf("%d", i);
-			printf(" ");
-		}
+			printf("%d ", i);
 	}
+
 	printf("\n");
-return (0);
+
+	return (0);
 }
