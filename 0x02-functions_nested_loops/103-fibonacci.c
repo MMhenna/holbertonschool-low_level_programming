895789b376787edb5637759a7029a7adf801b9d1
MMhenna <mhenna.mehdi@gmail.com>
2021-02-13 11:52:49 +0100

    fix fibo2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,17 +8,19 @@
 
 int main(void)
 {
-	int prev1 = 1, prev2 = 2;
-	long int sum = 0, s = 0;
+	int prev1 = 1, prev2 = 2, sum = 0, s = 0;
 
-	while (s <= 4000000)
+	while (s < 4000000)
 	{
 		s = prev1 + prev2;
-		sum += s;
+
+		if (s % 2 == 0)
+			sum += s;
+
 		prev1 = prev2;
 		prev2 = s;
 	}
-	printf("%li\n", sum);
+	printf("%d\n", sum);
 return (0);
 }
 
