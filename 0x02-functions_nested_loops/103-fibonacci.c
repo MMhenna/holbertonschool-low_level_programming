705a5e7ffcc6d2b7646080c7044330e7be07ffb0
MMhenna <mhenna.mehdi@gmail.com>
2021-02-13 11:40:25 +0100

    fibo 2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ * prints the sum of the even-valued terms.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int prev1 = 1, prev2 = 2, sum = 3, s = 0;
+
+	while (s <= 4000000)
+	{
+		s = prev1 + prev2;
+		sum += s;
+		prev1 = prev2;
+		prev2 = s;
+	}
+	printf("%d\n", sum);
+return (0);
+}
+
