48c9571c0c0f8bf6c8df3a6a4447a063b8f04d81
MMhenna <mhenna.mehdi@gmail.com>
2021-02-16 14:00:24 +0100

    convert st to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_atoi - convert string to int
+ *@s: the string to convert
+ * Return: success
+ **/
+
+int _atoi(char *s)
+{
+	unsigned int nbr = 0;
+	int j = 0, cp = 0, cm = 0;
+
+	while (((s[j] < '0') || (s[j] > '9')) && (s[j] != '\0'))
+	{
+		j++;
+		if (s[j] == '-')
+			cm++;
+		if (s[j] == '+')
+			cp++;
+	}
+
+	while (((s[j] >= '0') && (s[j] <= '9')) && (s[j] != '\0'))
+	{
+		if ((s[j] >= '0') && (s[j] <= '9'))
+		{
+			nbr = (nbr * 10) + (s[j] - '0');
+		}
+	j++;
+	}
+
+	if (cm > cp)
+		return (-nbr);
+	else
+		return (nbr);
+}
