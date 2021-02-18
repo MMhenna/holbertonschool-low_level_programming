275b58dbc1bbaa56e6f3b3fa150fac044eb70ba0
MMhenna <mhenna.mehdi@gmail.com>
2021-02-18 14:06:28 +0100

    fix 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  *reverse_array - reverse array
  *@a: array
@@ -11,8 +11,9 @@ void reverse_array(int *a, int n)
 {
 	int i, k;
 
-	for (i = 0; i <= n / 2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
+		printf("res = %d\n", a[i]);
 		k = a[i];
 		a[i] = a[(n - 1) - i];
 		a[(n - 1) - i] = k;
