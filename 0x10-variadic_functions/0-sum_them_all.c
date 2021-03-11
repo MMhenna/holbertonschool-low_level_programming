8401cccd24c189b464f86f9472a193d90fae260f
MMhenna <mhenna.mehdi@gmail.com>
2021-03-11 16:14:46 +0100

    fix 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,22 +4,21 @@
 /**
  *sum_them_all - return sum af numbers
  *@n: number of parameter
- *@...: parameters
  *
  * Return: sum.
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
+	unsigned int i;
+	int sum = 0;
 
 	va_list ap;
 
 	va_start(ap, n);
 
-	unsigned int i;
-	int sum = 0;
+	if (n == 0)
+		return (0);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
