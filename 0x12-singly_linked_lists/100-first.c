7c8536636cd68d9f13e71635d6c35be5f0d018ad
MMhenna <mhenna.mehdi@gmail.com>
2021-03-18 21:02:46 +0100

    add first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * beforeMain - Execute its contents before the main function is executed.
+ */
+
+void beforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
