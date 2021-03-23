1ef8329a031cafda5f19790b0f7ecc5924266d9c
MMhenna <mhenna.mehdi@gmail.com>
2021-03-23 17:39:54 +0100

    fix 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,14 +12,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i;
 
-	if (h == NULL)
-		return (0);
-
 	for (i = 0; h; i++)
 	{
 		printf("%i\n", h->n);
 		h = h->next;
 	}
+
 	return (i);
 }
 
