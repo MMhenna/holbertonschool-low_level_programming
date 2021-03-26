e5dcb1be02191afac6ba44520fa71be22a3c7dcd
MMhenna <mhenna.mehdi@gmail.com>
2021-03-26 12:05:25 +0100

    fix 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,16 +10,9 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-		_putchar('0');
-
-	else
-	{
+	if (n > 1)
 		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 
-		if ((n & 1) == 1)
-			_putchar('1');
-		if ((n & 1) == 0)
-			_putchar('0');
-	}
+
 }
