d33f7ea13b960066ea9bce05fcc22cd6702be312
MMhenna <mhenna.mehdi@gmail.com>
2021-05-28 14:40:35 +0100

    fix 0 - 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,29 +9,29 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *table;
-        unsigned long int i;
+	hash_table_t *table;
+	unsigned long int i;
 
-        if (size <= 0)
-        	return (NULL);
+	if (size <= 0)
+		return (NULL);
 
-        table = malloc(sizeof(hash_table_t));
-        if (table == NULL)
-        	return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 
-        table->size = size;
-        table->array = malloc(sizeof(hash_node_t *) * size);
-        if (table->array == NULL)
-        {
-        	free(table);
-        	return (NULL);
-        }
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
-        i = 0;
-        while (i < size)
-        {
+	i = 0;
+	while (i < size)
+	{
         	table->array[i] = NULL;
         	++i;
         }
-        return (table);
+	return (table);
 }
