b73003b3782c25427ac985e681ce74727ca1177f
MMhenna <mhenna.mehdi@gmail.com>
2021-07-02 18:11:52 +0100

    commit

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """finds the perimeter of number of 0's around 1's in a grid"""
+    count = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                count += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    count -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    count -= 2
+return count
